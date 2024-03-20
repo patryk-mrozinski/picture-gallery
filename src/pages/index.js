9976e5fdc9ff245f227eabb2e81b880aca572d4d
patryk-mrozinski <patryk.mrozinski.pm@gmail.com>
2024-03-20 22:46:35 +0100

    add pictures and styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import { GatsbyImage } from 'gatsby-plugin-image';
+import * as styles from './gallery.module.css';
 
 const IndexPage = ({ data }) => {
   return (
     <div>
       <h1>Picture Gallery</h1>
-      <div>
+
+      <div className={styles.galleryContainer}>
         {data.allFile.nodes.map((node) => (
-          <GatsbyImage image={node.childImageSharp.gatsbyImageData} alt={node.name} key={node.id} />
+          <div className={styles.imageWrapper} key={node.id}>
+            <GatsbyImage image={node.childImageSharp.gatsbyImageData} alt={node.name} />
+          </div>
         ))}
       </div>
     </div>
