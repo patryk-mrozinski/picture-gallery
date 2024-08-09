4d95df5bd1cc99696c2f01c3155e0fba084f4fda
patryk-mrozinski <patryk.mrozinski.pm@gmail.com>
2024-08-09 17:09:08 +0200

    small clean up pf the component, remove react-photo-view

--- src/components/image-gallery.js
+++ src/components/image-gallery.js
@@ -1,23 +1,28 @@
-import React, { Component, useEffect } from "react"
+import * as React from "react"
 import LightGallery from "lightgallery/react"
 
 import "lightgallery/css/lightgallery.css"
-import "lightgallery/css/lg-zoom.css"
 import "lightgallery/css/lg-thumbnail.css"
 
-// Import plugins if needed
-import lgZoom from "lightgallery/plugins/zoom"
 import lgThumbnail from "lightgallery/plugins/thumbnail"
-
-import "react-photo-view/dist/react-photo-view.css"
 
 const ImageGallery = props => {
   const images = props.images
 
   return (
-    <LightGallery plugins={[lgZoom, lgThumbnail]} speed={500} thumbnail={true}>
+    <LightGallery
+      plugins={[lgThumbnail]}
+      speed={500}
+      thumbnail={true}
+      download={false}
+    >
       {images.map((image, index) => (
-        <a href={image.secure_url} key={index} className="image-preview-link">
+        <a
+          href={image.secure_url}
+          key={index}
+          className="image-preview-link"
+          data-sub-html={`fsafsfas ${index}`}
+        >
           <img
             className="object-cover w-full h-full cursor-pointer"
             src={image.secure_url}
