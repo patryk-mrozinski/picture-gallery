06def8455055c438cdff5560a8306e45279b873c
patryk-mrozinski <patryk.mrozinski.pm@gmail.com>
2024-08-14 18:09:26 +0200

    background image on about page + making it responsive

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,6 +3,7 @@ import { graphql, Link } from "gatsby"
 
 import { LanguageContext } from "../context/LanguageContext"
 import LanguageContextButton from "../components/language-context-button"
+import { StaticImage } from "gatsby-plugin-image"
 
 const About = ({ data }) => {
   const { selectedLanguage, isSelectedLanguageEnglish } =
@@ -14,15 +15,31 @@ const About = ({ data }) => {
 
   return (
     <div>
-      <Link
-        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-        to="/"
-      >
-        {isSelectedLanguageEnglish ? "Go back to gallery" : "Wroc do galerii"}
-      </Link>
-      <LanguageContextButton />
-      <div className="flex flex-wrap my-10">
-        <p>{aboutText.node.frontmatter.about}</p>
+      <div class="grid grid-cols-10">
+        <Link
+          className="col-start-1 col-span-3 lg:col-span-1 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+          to="/"
+        >
+          {isSelectedLanguageEnglish ? "Go back to gallery" : "Wroc do galerii"}
+        </Link>
+        <div class="col-start-6 lg:col-start-9">
+          <LanguageContextButton />
+        </div>
+      </div>
+
+      <div className="relative h-screen mx-2 my-4">
+        <StaticImage
+          src="../markdown/images/DSC06526.JPG"
+          alt="Background"
+          layout="fullWidth"
+          className="absolute inset-0 w-full h-screen object-cover rounded-lg"
+        />
+
+        <div className="absolute inset-0 overflow-y-scroll content-center">
+          <div className="w-full p-4 text-white flex items-center justify-center">
+            <p className="">{aboutText.node.frontmatter.about}</p>
+          </div>
+        </div>
       </div>
     </div>
   )
